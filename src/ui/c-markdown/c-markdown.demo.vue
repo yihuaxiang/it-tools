324fc84828d15e36c34799f23aad8b7dbcc06ba3
fudongdong <fudongdonguestc@gmail.com>
2024-04-05 13:06:23 +0800

    chore

--- src/ui/c-markdown/c-markdown.demo.vue
+++ src/ui/c-markdown/c-markdown.demo.vue
@@ -9,7 +9,7 @@ mollis blandit, nunc nisl aliquam nunc, vitae aliquam nisl nunc vitae nisl.
 - Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 - Sed euismod, nisl quis mollis blandit, nunc nisl aliquam nunc, vitae aliquam nisl nunc vitae nisl.
 
-[it-tools](https://it-tools.tech)
+[it-tools](https://fukit.cn/)
 `;
 </script>
 
